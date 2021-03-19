1422190445038aae30be17e8c94954a48ad9c733
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-19 15:11:58 -0500

    Created Customer Model with variables based on UserStories.

---
+++ TrashCollectorMVP/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorMVP.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+
+        [ForeignKey("WeeklyPickupDay")]
+        public int WeeklyPickUpDayId { get; set; }
+        public WeeklyPickupDay WeeklyPickUpDay { get; set; }
+    }
+}
