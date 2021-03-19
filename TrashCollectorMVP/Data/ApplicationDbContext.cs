0a30d2426fc0ec5be5a7d9072f40f912273325b1
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-19 15:27:29 -0500

    Added Migration to update database with tables.

--- TrashCollectorMVP/Data/ApplicationDbContext.cs
+++ TrashCollectorMVP/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TrashCollectorMVP.Models;
 
 namespace TrashCollectorMVP.Data
 {
@@ -13,7 +14,9 @@ namespace TrashCollectorMVP.Data
             : base(options)
         {
         }
-
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<WeeklyPickupDay> WeeklyPickupDays { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -30,6 +33,43 @@ namespace TrashCollectorMVP.Data
                 NormalizedName = "EMPLOYEE"
             }
             );
+            builder.Entity<WeeklyPickupDay>()
+                .HasData(
+            new WeeklyPickupDay
+            {
+                Id = 1,
+                Day = "Sunday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 2,
+                Day = "Monday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 3,
+                Day = "Tuesday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 4,
+                Day = "Wednesday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 5,
+                Day = "Thursday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 6,
+                Day = "Friday"
+            },
+            new WeeklyPickupDay
+            {
+                Id = 7,
+                Day = "Saturday"
+            });
         }
     }
 }
