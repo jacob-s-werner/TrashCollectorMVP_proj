cb5820ef07c5ff29fe66df37f9e6665029ed3b34
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-19 15:18:36 -0500

    Created Employee and WeeklyPickupDay Models, changed Customer model.

---
+++ TrashCollectorMVP/Models/WeeklyPickupDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollectorMVP.Models
+{
+    public class WeeklyPickupDay
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Day { get; set; }
+    }
+}
