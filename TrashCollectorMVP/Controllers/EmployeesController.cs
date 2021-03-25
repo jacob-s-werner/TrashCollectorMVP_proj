d537141dcdf993779d0727df5241113faf2d1e0a
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-24 21:27:46 -0500

    Added GoogleGeocoding from customer address to ConfirmOneTimePickup and ConfirmPickup.

--- TrashCollectorMVP/Controllers/EmployeesController.cs
+++ TrashCollectorMVP/Controllers/EmployeesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -237,12 +239,18 @@ namespace TrashCollectorMVP.Controllers
             {
                 return NotFound();
             }
-            
-            GeocodingRequest request = new GeocodingRequest(customer.Address +" "+ customer.ZipCode);
-            GeocodingResponse response = await request.GetResponseAsync();
-            LatLng location = response.Results[0].Geometry.Location;
-            customer.Latitude = location.Latitude;
-            customer.Longitude = location.Longitude;
+
+            string address = customer.Address;
+            string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(address), "AIzaSyBgx90LvORvn6zHVDRBehxL08wimqWfyTM");
+
+            WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
+            XDocument xdoc = XDocument.Load(response.GetResponseStream());
+
+            XElement result = xdoc.Element("GeocodeResponse").Element("result");
+            XElement locationElement = result.Element("geometry").Element("location");
+            customer.Latitude = (double)locationElement.Element("lat");
+            customer.Longitude = (double)locationElement.Element("lng");
 
             return View(customer);
         }
@@ -279,6 +287,19 @@ namespace TrashCollectorMVP.Controllers
                 return NotFound();
             }
             oneTimePickup = _context.OneTimePickups.Where(o => o.Id.Equals(id)).Include(c => c.Customer).FirstOrDefault();
+
+            string address = oneTimePickup.Customer.Address;
+            string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(address), "AIzaSyBgx90LvORvn6zHVDRBehxL08wimqWfyTM");
+
+            WebRequest request = WebRequest.Create(requestUri);
+            WebResponse response = request.GetResponse();
+            XDocument xdoc = XDocument.Load(response.GetResponseStream());
+
+            XElement result = xdoc.Element("GeocodeResponse").Element("result");
+            XElement locationElement = result.Element("geometry").Element("location");
+            oneTimePickup.Customer.Latitude = (double)locationElement.Element("lat");
+            oneTimePickup.Customer.Longitude = (double)locationElement.Element("lng");
+
             return View(oneTimePickup);
         }
 
